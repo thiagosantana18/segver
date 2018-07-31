4532844ad38e59bc890c128036b168a35043b073
Thiago Santana <thigoncalves64@gmail.com>
2018-07-31 09:56:22 -0300

    Adicionado as informações sobre a função Addslashes do PHP

--- index.php
+++ index.php
@@ -1,10 +1,24 @@
-<!DOCTYPE HTML>
+<!DOCTYPE html>
 <html lang="pt-br">
 <head>
-   <meta charset="uft-8">
-   <title>Aula Git</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Aula Git</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 </head>
 <body>
-   <h1>Arquivo a ser persistido no repositório Git</h1>
+    <div class="container">
+        <h1>Arquivo a ser persistido no repositório Git</h1>
+
+        <h3>Um exemplo de solução para proteção SQL Injection</h3>
+        <p>Addslashes - <em>Adiciona</em> barras invertidas a uma string</p>
+        <code>
+            <span style="color: blue">string</span> addslashes (<span style="color: blue">string</span><em>$query</em>)
+        </code>
+        <p><br>Retorna uma <em>string com barras invertidas</em> com os caracteres que precisam ser convertidos para string<br><strong>ex.:</strong> Sant'anna<br><strong>convertido:</strong> Sant'\anna'</p>
+    </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </body>
 </html>
